ccddc4955201f430a3457d7ec93ae0aa3e25030e
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-05-10 16:15:57 -0600

    First deployment

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
----
-name: HTML Starter
-slug: html-starter-with-analytics
-description: HTML5 template with analytics and advanced routing configuration.
-deployUrl: https://vercel.com/new/clone?repository-url=https://github.com/vercel/examples/tree/main/solutions/html&project-name=html
-relatedTemplates:
-  - nextjs-boilerplate
----
+# Rock, Paper, Scissors Game
 
-# HTML Starter
+A simple and interactive Rock, Paper, Scissors game built using HTML, CSS, and JavaScript.
 
-This is a starter HTML5 templates which is configured with Vercel Analytics (through a `script` tag), advanced routing with [Vercel Edge Middleware](https://vercel.com/docs/concepts/functions/edge-middleware), as well as some basic styles
+## Features
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/vercel/examples/tree/main/solutions/html&project-name=html)
+- Play Rock, Paper, Scissors against the computer.
+- Auto-play mode for continuous gameplay.
+- Reset the score with a confirmation modal.
+- Keyboard shortcuts for quick actions:
+  - **R**: Select Rock
+  - **P**: Select Paper
+  - **S**: Select Scissors
+  - **Backspace**: Reset Score
+  - **Y**: Yes button (modal)
+  - **N**: No Button (modal)
+  - **A**: Toggle Auto-Play and Stop 
+  - **Escape**: Close the confirmation modal.
+
+## Project Structure
---
+++ index.js
@@ -0,0 +1,184 @@
+// score record
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+updateScoreElement();
+
+document.querySelector(".js-result").innerHTML = `Start Playing!`;
+
+// game logic
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  const result = getResult(playerMove, computerMove);
+
+  updateScore(result);
+  saveScore();
+  updateScoreElement();
+  renderScore(playerMove, computerMove, result);
+}
+
+function pickComputerMove() {
+  const moves = ["rock", "paper", "scissors"];
+  const randomSelection = Math.floor(Math.random() * moves.length);
+
+  return moves[randomSelection];
+}
+
+function getResult(playerMove, computerMove) {
+  const outcomes = {
+    rock: { rock: "Tie.", paper: "You lose.", scissors: "You win." },
+    paper: { rock: "You win.", paper: "Tie.", scissors: "You lose." },
+    scissors: { rock: "You lose.", paper: "You win.", scissors: "Tie." },
+  };
+
+  return outcomes[playerMove][computerMove];
+}
+
+function updateScore(result) {
+  if (result === "You win.") {
+    score.wins += 1;
+  } else if (result === "You lose.") {
+    score.losses += 1;
+  } else if (result === "Tie.") {
+    score.ties += 1;
+  }
+}
+
+function saveScore() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
+
+function updateScoreElement() {
+  const { wins, losses, ties } = score;
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`;
+}
+
+function renderScore(playerMove, computerMove, result) {
+  document.querySelector(".js-result").innerHTML = `
+    ${result} <br><br>
+    You picked <img src='images/${playerMove}-emoji.png' class='move-icon'> 
+    Computer picked <img src='images/${computerMove}-emoji.png' class='move-icon'> `;
+}
+
+document
+  .querySelector(".rock-button")
+  .addEventListener("click", () => playGame("rock"));
+document
+  .querySelector(".paper-button")
+  .addEventListener("click", () => playGame("paper"));
+document
+  .querySelector(".scissors-button")
+  .addEventListener("click", () => playGame("scissors"));
+
+// autoplay button logic
+let isAutoPlaying = false;
+let intervalId;
+
+const autoplay = () => {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+    document.querySelector(".js-autoplay").innerHTML = "Stop autoplay";
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    document.querySelector(".js-autoplay").innerHTML = "Start autoplay";
+  }
+};
+
+document
+  .querySelector(".js-autoplay")
+  .addEventListener("click", () => autoplay());
+
+// reset button logic & confirmation modal
+const resetScore = () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+
+  localStorage.removeItem("score");
+  updateScoreElement();
+  document.querySelector(".js-result").innerHTML = `Start Playing!`;
+};
+
+const modal = document.querySelector(".modal-container");
+const modalButtons = document.querySelector(".modal-buttons");
+const yesButton = document.querySelector(".yes-modal-button");
+const noButton = document.querySelector(".no-modal-button");
+const modalMessage = document.querySelector(".modal-message");
+const closeButton = document.querySelector(".close-button");
+
+function resetTrigger(event) {
+  if (
+    event.type === "click" ||
+    (event.type === "keydown" && event.key === "Backspace")
+  ) {
+    const { wins, losses, ties } = score;
+
+    if (wins === 0 && losses === 0 && ties === 0) {
+      modalMessage.innerText = "You have no score!";
+      modalButtons.style.display = "none";
+    } else {
+      modalMessage.innerText = "Are you sure you want to reset the score?";
+      modalButtons.style.display = "flex";
+    }
+
+    modal.style.display = "flex";
+    modal.style.justifyContent = "center";
+    modal.style.alignItems = "center";
+  }
+}
+
+document
+  .querySelector(".reset-button")
+  .addEventListener("click", (event) => resetTrigger(event));
+document.addEventListener("keydown", (event) => resetTrigger(event));
+
+yesButton.addEventListener("click", () => {
+  resetScore();
+  modal.style.display = "none";
+});
+
+noButton.addEventListener("click", () => (modal.style.display = "none"));
+
+closeButton.addEventListener("click", () => (modal.style.display = "none"));
+
+// keydown events
+document.addEventListener("keydown", (event) => {
+  if (event.key == "a") autoplay();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "r") playGame("rock");
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "p") playGame("paper");
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "s") playGame("scissors");
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  const isModalOpen = modal.style.display === "flex";
+  if (isModalOpen) {
+    if (event.key === "y") {
+      yesButton.click();
+    } else if (event.key === "n") {
+      noButton.click();
+    }
+  }
+});
